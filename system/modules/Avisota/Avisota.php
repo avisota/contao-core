8bd1edd42f1d82abc988ab8026ebe9f597bd2062
Tristan Lins <tristan.lins@infinitysoft.de>
2011-04-19 20:17:11 +0200

    Support embeded background images.

--- system/modules/Avisota/Avisota.php
+++ system/modules/Avisota/Avisota.php
@@ -918,7 +918,7 @@ class Avisota extends BackendModule
 	 */
 	protected function generateEmailObject(Database_Result &$objNewsletter, Database_Result &$objCategory, $arrAttachments)
 	{
-		$objEmail = new Email();
+		$objEmail = new AvisotaEmail();
 
 		$objEmail->from = $objCategory->sender;
 		$objEmail->subject = $objNewsletter->subject;
