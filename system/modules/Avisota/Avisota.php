a2b33e9a1fbedc591c74eba347f7052014120124
Tristan Lins <tristan.lins@infinitysoft.de>
2011-05-05 22:16:56 +0200

    Rename AvisotaEmail class to ExtendedEmail to prevent conflicts with avisota_email extension.

--- system/modules/Avisota/Avisota.php
+++ system/modules/Avisota/Avisota.php
@@ -948,7 +948,7 @@ class Avisota extends BackendModule
 	 */
 	protected function generateEmailObject(Database_Result &$objNewsletter, Database_Result &$objCategory, $arrAttachments)
 	{
-		$objEmail = new AvisotaEmail();
+		$objEmail = new ExtendedEmail();
 
 		$objEmail->from = $objCategory->sender;
 		$objEmail->subject = $objNewsletter->subject;
