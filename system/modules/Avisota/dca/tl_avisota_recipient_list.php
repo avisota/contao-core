a9de5e8306ca4129462798e4ea1cc7b1097ad7fc
Tristan Lins <tristan.lins@infinitysoft.de>
2011-06-25 10:12:06 +0200

    Implement permission for recipient lists.

--- system/modules/Avisota/dca/tl_avisota_recipient_list.php
+++ system/modules/Avisota/dca/tl_avisota_recipient_list.php
@@ -45,7 +45,11 @@ $GLOBALS['TL_DCA']['tl_avisota_recipient_list'] = array
 		'dataContainer'               => 'Table',
 		'ctable'                      => array('tl_avisota_recipient'),
 		'switchToEdit'                => true,
-		'enableVersioning'            => true
+		'enableVersioning'            => true,
+		'onload_callback' => array
+		(
+			array('tl_avisota_recipient_list', 'checkPermission')
+		)
 	),
 
 	// List
@@ -87,6 +91,7 @@ $GLOBALS['TL_DCA']['tl_avisota_recipient_list'] = array
 				'label'               => &$GLOBALS['TL_LANG']['tl_avisota_recipient_list']['editheader'],
 				'href'                => 'act=edit',
 				'icon'                => 'header.gif',
+				'button_callback'     => array('tl_avisota_recipient_list', 'editHeader'),
 				'attributes'          => 'class="edit-header"'
 			),
 			'copy' => array
@@ -94,14 +99,16 @@ $GLOBALS['TL_DCA']['tl_avisota_recipient_list'] = array
 				'label'               => &$GLOBALS['TL_LANG']['tl_avisota_recipient_list']['copy'],
 				'href'                => 'act=copy',
 				'icon'                => 'copy.gif',
-				'attributes'          => 'onclick="Backend.getScrollOffset();"'
+				'attributes'          => 'onclick="Backend.getScrollOffset();"',
+				'button_callback'     => array('tl_avisota_recipient_list', 'copyCategory')
 			),
 			'delete' => array
 			(
 				'label'               => &$GLOBALS['TL_LANG']['tl_avisota_recipient_list']['delete'],
 				'href'                => 'act=delete',
 				'icon'                => 'delete.gif',
-				'attributes'          => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"'
+				'attributes'          => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"',
+				'button_callback'     => array('tl_avisota_recipient_list', 'deleteCategory')
 			),
 			'show' => array
 			(
@@ -160,6 +167,189 @@ $GLOBALS['TL_DCA']['tl_avisota_recipient_list'] = array
 
 class tl_avisota_recipient_list extends Backend
 {
+	/**
+	 * Import the back end user object
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->import('BackendUser', 'User');
+	}
+
+
+	/**
+	 * Check permissions to edit table tl_newsletter_channel
+	 */
+	public function checkPermission()
+	{
+		if ($this->User->isAdmin)
+		{
+			return;
+		}
+
+		// Set root IDs
+		if (!is_array($this->User->avisota_recipient_lists) || count($this->User->avisota_recipient_lists) < 1)
+		{
+			$root = array(0);
+		}
+		else
+		{
+			$root = $this->User->avisota_recipient_lists;
+		}
+
+		$GLOBALS['TL_DCA']['tl_avisota_recipient_list']['list']['sorting']['root'] = $root;
+
+		// Check permissions to add recipient lists
+		if (!$this->User->hasAccess('create', 'avisota_recipient_list_permissions'))
+		{
+			$GLOBALS['TL_DCA']['tl_avisota_recipient_list']['config']['closed'] = true;
+		}
+
+		// Check current action
+		switch ($this->Input->get('act'))
+		{
+			case 'create':
+			case 'select':
+				// Allow
+				break;
+
+			case 'edit':
+				// Dynamically add the record to the user profile
+				if (!in_array($this->Input->get('id'), $root))
+				{
+					$arrNew = $this->Session->get('new_records');
+
+					if (is_array($arrNew['tl_avisota_recipient_list']) && in_array($this->Input->get('id'), $arrNew['tl_avisota_recipient_list']))
+					{
+						// Add permissions on user level
+						if ($this->User->inherit == 'custom' || !$this->User->groups[0])
+						{
+							$objUser = $this->Database->prepare("SELECT avisota_recipient_lists, avisota_recipient_list_permissions FROM tl_user WHERE id=?")
+													   ->limit(1)
+													   ->execute($this->User->id);
+
+							$arrNewsletterCategoryPermissions = deserialize($objUser->avisota_recipient_list_permissions);
+
+							if (is_array($arrNewsletterCategoryPermissions) && in_array('create', $arrNewsletterCategoryPermissions))
+							{
+								$arrNewsletterCategories = deserialize($objUser->avisota_recipient_lists);
+								$arrNewsletterCategories[] = $this->Input->get('id');
+
+								$this->Database->prepare("UPDATE tl_user SET avisota_recipient_lists=? WHERE id=?")
+											   ->execute(serialize($arrNewsletterCategories), $this->User->id);
+							}
+						}
+
+						// Add permissions on group level
+						elseif ($this->User->groups[0] > 0)
+						{
+							$objGroup = $this->Database->prepare("SELECT avisota_recipient_lists, avisota_recipient_list_permissions FROM tl_user_group WHERE id=?")
+													   ->limit(1)
+													   ->execute($this->User->groups[0]);
+
+							$arrNewsletterCategoryPermissions = deserialize($objGroup->avisota_recipient_list_permissions);
+
+							if (is_array($arrNewsletterCategoryPermissions) && in_array('create', $arrNewsletterCategoryPermissions))
+							{
+								$arrNewsletterCategories = deserialize($objGroup->avisota_recipient_lists);
+								$arrNewsletterCategories[] = $this->Input->get('id');
+
+								$this->Database->prepare("UPDATE tl_user_group SET avisota_recipient_lists=? WHERE id=?")
+											   ->execute(serialize($arrNewsletterCategories), $this->User->groups[0]);
+							}
+						}
+
+						// Add new element to the user object
+						$root[] = $this->Input->get('id');
+						$this->User->avisota_recipient_lists = $root;
+					}
+				}
+				// No break;
+
+			case 'copy':
+			case 'delete':
+			case 'show':
+				if (!in_array($this->Input->get('id'), $root) || ($this->Input->get('act') == 'delete' && !$this->User->hasAccess('delete', 'avisota_recipient_list_permissions')))
+				{
+					$this->log('Not enough permissions to '.$this->Input->get('act').' avisota newsletter category ID "'.$this->Input->get('id').'"', 'tl_avisota_recipient_list checkPermission', TL_ERROR);
+					$this->redirect('contao/main.php?act=error');
+				}
+				break;
+
+			case 'editAll':
+			case 'deleteAll':
+			case 'overrideAll':
+				$session = $this->Session->getData();
+				if ($this->Input->get('act') == 'deleteAll' && !$this->User->hasAccess('delete', 'avisota_recipient_list_permissions'))
+				{
+					$session['CURRENT']['IDS'] = array();
+				}
+				else
+				{
+					$session['CURRENT']['IDS'] = array_intersect($session['CURRENT']['IDS'], $root);
+				}
+				$this->Session->setData($session);
+				break;
+
+			default:
+				if (strlen($this->Input->get('act')))
+				{
+					$this->log('Not enough permissions to '.$this->Input->get('act').' avisota newsletter categories', 'tl_avisota_recipient_list checkPermission', TL_ERROR);
+					$this->redirect('contao/main.php?act=error');
+				}
+				break;
+		}
+	}
+
+
+	/**
+	 * Return the edit header button
+	 * @param array
+	 * @param string
+	 * @param string
+	 * @param string
+	 * @param string
+	 * @param string
+	 * @return string
+	 */
+	public function editHeader($row, $href, $label, $title, $icon, $attributes)
+	{
+		return ($this->User->isAdmin || count(preg_grep('/^tl_avisota_recipient_list::/', $this->User->alexf)) > 0) ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.specialchars($title).'"'.$attributes.'>'.$this->generateImage($icon, $label).'</a> ' : '';
+	}
+
+
+	/**
+	 * Return the copy channel button
+	 * @param array
+	 * @param string
+	 * @param string
+	 * @param string
+	 * @param string
+	 * @param string
+	 * @return string
+	 */
+	public function copyCategory($row, $href, $label, $title, $icon, $attributes)
+	{
+		return ($this->User->isAdmin || $this->User->hasAccess('create', 'avisota_recipient_list_permissions')) ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.specialchars($title).'"'.$attributes.'>'.$this->generateImage($icon, $label).'</a> ' : $this->generateImage(preg_replace('/\.gif$/i', '_.gif', $icon)).' ';
+	}
+
+
+	/**
+	 * Return the delete channel button
+	 * @param array
+	 * @param string
+	 * @param string
+	 * @param string
+	 * @param string
+	 * @param string
+	 * @return string
+	 */
+	public function deleteCategory($row, $href, $label, $title, $icon, $attributes)
+	{
+		return ($this->User->isAdmin || $this->User->hasAccess('delete', 'avisota_recipient_list_permissions')) ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.specialchars($title).'"'.$attributes.'>'.$this->generateImage($icon, $label).'</a> ' : $this->generateImage(preg_replace('/\.gif$/i', '_.gif', $icon)).' ';
+	}
+	
+	
 	/**
 	 * Autogenerate a news alias if it has not been set yet
 	 * @param mixed
