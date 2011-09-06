8f34cff39291e2fddc8dbca0875e3c80a606381a
Tristan Lins <tristan.lins@infinitysoft.de>
2011-09-06 15:52:12 +0200

    Fix patch issue.

--- system/modules/Avisota/AvisotaRegistrationDCA.php
+++ system/modules/Avisota/AvisotaRegistrationDCA.php
@@ -63,20 +63,20 @@ class AvisotaRegistrationDCA extends Controller {
 		if(!$arrLists)
 			return;
 
-		$objStmt = $this->Database->prepare('');
+		$objPrepared = $this->Database->prepare('
+			SELECT	r.confirmed, r.id AS rid
+			FROM	tl_avisota_recipient_list AS l
+			LEFT JOIN (
+				SELECT	r1.confirmed, r1.id, r1.pid
+				FROM	tl_avisota_recipient AS r1
+				WHERE	r1.email = ?
+			) AS r ON l.id = r.pid
+			WHERE l.id = ?
+		');
 		$intTime = time();
 
 		foreach($arrLists as $intListID) {
-			$objAlreadySubscribed = $objStmt->prepare('
-				SELECT	r.confirmed, r.id AS rid
-				FROM	tl_avisota_recipient_list AS l
-				LEFT JOIN (
-					SELECT	r1.confirmed, r1.id, r1.pid
-					FROM	tl_avisota_recipient AS r1
-					WHERE	r1.email = ?
-				) AS r ON l.id = r.pid
-				WHERE l.id = ?
-			')->execute($strEmail, $intListID);
+			$objAlreadySubscribed = $objPrepared->execute($strEmail, $intListID);
 
 			if(!$objAlreadySubscribed->numRows) // list doesnt exist
 				continue;
