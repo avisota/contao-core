84723f827abbcd13c89e0c1d7630d7460c81111c
Tristan Lins <tristan.lins@bit3.de>
2013-07-17 23:03:54 +0200

    WIP Implementation of modular renderer chain.

---
+++ src/Avisota/Contao/Message/Renderer/MessageRendererInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota
+ * @license    LGPL
+ * @filesource
+ */
+
+namespace Avisota\Contao\Message\Renderer;
+
+use Avisota\Contao\Entity\Message;
+use Avisota\Recipient\RecipientInterface;
+
+interface MessageRendererInterface
+{
+    public function render(Message $message, RecipientInterface $recipient = null);
+}
