419d430ed9b35880e1ce15007e1895e45dd9b00a
Tristan Lins <tristan.lins@bit3.de>
2014-03-11 19:46:52 +0100

    Convert exceptions to errors in service factory.
    Exceptions may break the system, so convert into errors warnings to
    just notify about the issue.

--- src/Avisota/Contao/Core/ServiceFactory.php
+++ src/Avisota/Contao/Core/ServiceFactory.php
@@ -36,56 +36,71 @@ class ServiceFactory
 		$container['doctrine.orm.entityManager'];
 
 		if (class_exists('Avisota\Contao\Entity\RecipientSource')) {
-			$recipientSourceRepository = EntityHelper::getRepository('Avisota\Contao:RecipientSource');
-			/** @var RecipientSource[] $recipientSources */
-			$recipientSources = $recipientSourceRepository->findAll();
+			try {
+				$recipientSourceRepository = EntityHelper::getRepository('Avisota\Contao:RecipientSource');
+				/** @var RecipientSource[] $recipientSources */
+				$recipientSources = $recipientSourceRepository->findAll();
 
-			foreach ($recipientSources as $recipientSource) {
-				$container[sprintf('avisota.recipientSource.%s', $recipientSource->getId())] = $container->share(
-					function ($container) use ($recipientSource, $factory) {
-						return $factory->createRecipientSource($recipientSource);
-					}
-				);
+				foreach ($recipientSources as $recipientSource) {
+					$container[sprintf('avisota.recipientSource.%s', $recipientSource->getId())] = $container->share(
+						function ($container) use ($recipientSource, $factory) {
+							return $factory->createRecipientSource($recipientSource);
+						}
+					);
 
-				$container[sprintf('avisota.recipientSource.%s', $recipientSource->getAlias())] = function($container) use ($recipientSource) {
-					return $container[sprintf('avisota.recipientSource.%s', $recipientSource->getId())];
-				};
+					$container[sprintf('avisota.recipientSource.%s', $recipientSource->getAlias())] = function($container) use ($recipientSource) {
+						return $container[sprintf('avisota.recipientSource.%s', $recipientSource->getId())];
+					};
+				}
+			}
+			catch (\Exception $e) {
+				trigger_error($e->getMessage(), E_USER_WARNING);
 			}
 		}
 
 		if (class_exists('Avisota\Contao\Entity\Queue')) {
-			$queueRepository = EntityHelper::getRepository('Avisota\Contao:Queue');
-			/** @var Queue[] $queues */
-			$queues = $queueRepository->findAll();
+			try {
+				$queueRepository = EntityHelper::getRepository('Avisota\Contao:Queue');
+				/** @var Queue[] $queues */
+				$queues = $queueRepository->findAll();
 
-			foreach ($queues as $queue) {
-				$container[sprintf('avisota.queue.%s', $queue->getId())] = $container->share(
-					function ($container) use ($queue, $factory) {
-						return $factory->createQueue($queue);
-					}
-				);
+				foreach ($queues as $queue) {
+					$container[sprintf('avisota.queue.%s', $queue->getId())] = $container->share(
+						function ($container) use ($queue, $factory) {
+							return $factory->createQueue($queue);
+						}
+					);
 
-				$container[sprintf('avisota.queue.%s', $queue->getAlias())] = function($container) use ($queue) {
-					return $container[sprintf('avisota.queue.%s', $queue->getId())];
-				};
+					$container[sprintf('avisota.queue.%s', $queue->getAlias())] = function($container) use ($queue) {
+						return $container[sprintf('avisota.queue.%s', $queue->getId())];
+					};
+				}
+			}
+			catch (\Exception $e) {
+				trigger_error($e->getMessage(), E_USER_WARNING);
 			}
 		}
 
 		if (class_exists('Avisota\Contao\Entity\Transport')) {
-			$transportRepository = EntityHelper::getRepository('Avisota\Contao:Transport');
-			/** @var Transport[] $transports */
-			$transports = $transportRepository->findAll();
+			try {
+				$transportRepository = EntityHelper::getRepository('Avisota\Contao:Transport');
+				/** @var Transport[] $transports */
+				$transports = $transportRepository->findAll();
 
-			foreach ($transports as $transport) {
-				$container[sprintf('avisota.transport.%s', $transport->getId())] = $container->share(
-					function ($container) use ($transport, $factory) {
-						return $factory->createTransport($transport);
-					}
-				);
+				foreach ($transports as $transport) {
+					$container[sprintf('avisota.transport.%s', $transport->getId())] = $container->share(
+						function ($container) use ($transport, $factory) {
+							return $factory->createTransport($transport);
+						}
+					);
 
-				$container[sprintf('avisota.transport.%s', $transport->getAlias())] = function($container) use ($transport) {
-					return $container[sprintf('avisota.transport.%s', $transport->getId())];
-				};
+					$container[sprintf('avisota.transport.%s', $transport->getAlias())] = function($container) use ($transport) {
+						return $container[sprintf('avisota.transport.%s', $transport->getId())];
+					};
+				}
+			}
+			catch (\Exception $e) {
+				trigger_error($e->getMessage(), E_USER_WARNING);
 			}
 		}
 	}
