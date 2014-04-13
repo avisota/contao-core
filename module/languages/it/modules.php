0c4463ab3559e87b440094a2003b4adfaba889f0
Avibot <avibot@avisota.org>
2014-04-13 04:01:02 +0200

    Auto-Update translations from transifex

--- module/languages/it/modules.php
+++ module/languages/it/modules.php
@@ -11,12 +11,13 @@
  * last-updated: 2014-04-07T04:03:00+02:00
  */
 
-
 $GLOBALS['TL_LANG']['FMD']['avisota']                   = 'Avisota';
 $GLOBALS['TL_LANG']['MOD']['avisota']['0']              = 'Newsletter Avisota';
 $GLOBALS['TL_LANG']['MOD']['avisota_config']['0']       = 'Impostazioni';
 $GLOBALS['TL_LANG']['MOD']['avisota_mailing_list']['0'] = 'Mailing lists';
 $GLOBALS['TL_LANG']['MOD']['avisota_newsletter']['0']   = 'Messaggi';
+$GLOBALS['TL_LANG']['MOD']['avisota_queue']['0']        = 'Code';
+$GLOBALS['TL_LANG']['MOD']['avisota_recipients']['0']   = 'Destinatari';
 $GLOBALS['TL_LANG']['MOD']['avisota_support']['0']      = 'Supporto';
 $GLOBALS['TL_LANG']['MOD']['avisota_theme']['0']        = 'Tema';
 
