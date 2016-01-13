0d94c6d155f1570de0c8142cbb0e82404e078e1e
Sven Baumann <baumann.sv@gmail.com>
2016-01-13 13:04:53 +0100

    fix extend static method with non static method

--- src/Avisota/Contao/Core/ReplaceInsertTagsHook.php
+++ src/Avisota/Contao/Core/ReplaceInsertTagsHook.php
@@ -42,7 +42,7 @@ class ReplaceInsertTagsHook extends \Controller
 	 *
 	 * @return string The text with the replaced tags
      */
-	public function replaceInsertTags($strBuffer, $blnCache = false)
+	public static function replaceInsertTags($strBuffer, $blnCache = false)
 	{
 		return parent::replaceInsertTags($strBuffer, $blnCache);
 	}
