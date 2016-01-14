513b0280dd43947286554e6b4eef71beba9a5d20
Sven Baumann <baumann.sv@gmail.com>
2016-01-14 01:26:33 +0100

    optimize imports

--- src/Avisota/Contao/Core/ServiceFactory.php
+++ src/Avisota/Contao/Core/ServiceFactory.php
@@ -15,12 +15,12 @@
 
 namespace Avisota\Contao\Core;
 
+use Avisota\Contao\Core\Queue\QueueFactoryInterface;
 use Avisota\Contao\Core\RecipientSource\RecipientSourceFactoryInterface;
 use Avisota\Contao\Core\Transport\TransportFactoryInterface;
-use Avisota\Contao\Core\Queue\QueueFactoryInterface;
+use Avisota\Contao\Entity\Queue;
 use Avisota\Contao\Entity\RecipientSource;
 use Avisota\Contao\Entity\Transport;
-use Avisota\Contao\Entity\Queue;
 use Contao\Doctrine\ORM\EntityHelper;
 
 /**
