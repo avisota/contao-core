d22220d58f16c512e064595b88347bccd20ffa65
Sven Baumann <baumann.sv@gmail.com>
2016-02-19 12:14:44 +0100

    add handling to copy elements with child's  complete

--- src/Controller/CopyController.php
+++ src/Controller/CopyController.php
@@ -15,9 +15,16 @@
 
 namespace Avisota\Contao\Core\Controller;
 
+use ContaoCommunityAlliance\Contao\Bindings\ContaoEvents;
+use ContaoCommunityAlliance\Contao\Bindings\Events\Controller\RedirectEvent;
+use ContaoCommunityAlliance\DcGeneral\Contao\View\Contao2BackendView\ActionHandler\CopyHandler;
 use ContaoCommunityAlliance\DcGeneral\Data\ModelId;
+use ContaoCommunityAlliance\DcGeneral\Data\ModelIdInterface;
+use ContaoCommunityAlliance\DcGeneral\Data\ModelInterface;
 use ContaoCommunityAlliance\DcGeneral\DcGeneralEvents;
+use ContaoCommunityAlliance\DcGeneral\EnvironmentInterface;
 use ContaoCommunityAlliance\DcGeneral\Event\ActionEvent;
+use ContaoCommunityAlliance\UrlBuilder\Contao\BackendUrlBuilder;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -52,6 +59,7 @@ class CopyController implements EventSubscriberInterface
         return array(
             DcGeneralEvents::ACTION => array(
                 array('handleEdit', 20),
+                array('handleCopyChilds'),
             ),
         );
     }
@@ -99,8 +107,166 @@ class CopyController implements EventSubscriberInterface
         $parentModel            = $parentDataProvider->fetch(
             $parentDataProvider->getEmptyConfig()->setId($model->getEntity()->$parentPropertyMethod()->getId())
         );
-        $parentModelId = ModelId::fromModel($parentModel);
+        $parentModelId          = ModelId::fromModel($parentModel);
 
         \Input::setGet('pid', $parentModelId->getSerialized());
+    }
+
+    /**
+     * @param ActionEvent     $event
+     * @param                 $name
+     * @param EventDispatcher $eventDispatcher
+     */
+    public function handleCopyChilds(ActionEvent $event, $name, EventDispatcher $eventDispatcher)
+    {
+        if ($event->getAction()->getName() !== 'copyChilds' || !\Input::get('ctable')) {
+            return;
+        }
+
+        $environment   = $event->getEnvironment();
+        $inputProvider = $environment->getInputProvider();
+        $modelId       = ModelId::fromSerialized($inputProvider->getParameter('id'));
+        if ($environment->getDataDefinition()->getName() !== $inputProvider->getParameter('ctable')
+            && $environment->getParentDataDefinition()->getName() !== $modelId->getDataProviderName()
+        ) {
+            $this->redirectToChildTable($environment);
+        }
+
+        $copiedParentModel = $this->copyParent($modelId, $environment);
+
+        $this->copyEachChilds($modelId, $copiedParentModel, $environment);
+
+        $this->redirectToChildTable($environment, true);
+    }
+
+    /**
+     * @param EnvironmentInterface $environment
+     * @param bool                 $doNotCopy
+     */
+    protected function redirectToChildTable(EnvironmentInterface $environment, $doNotCopy = false)
+    {
+        $redirectUrl = new BackendUrlBuilder();
+        $redirectUrl
+            ->setPath('contao/main.php')
+            ->setQueryParameter('do', $environment->getInputProvider()->getParameter('do'));
+
+        if ($doNotCopy === false) {
+            $redirectUrl
+                ->setQueryParameter('act', $environment->getInputProvider()->getParameter('act'))
+                ->setQueryParameter('ctable', $environment->getInputProvider()->getParameter('ctable'))
+                ->setQueryParameter('table', $environment->getInputProvider()->getParameter('ctable'))
+                ->setQueryParameter('id', $environment->getInputProvider()->getParameter('id'))
+                ->setQueryParameter('pid', $environment->getInputProvider()->getParameter('id'));
+        }
+
+        if ($doNotCopy) {
+            $redirectUrl
+                ->setQueryParameter('act', 'edit')
+                ->setQueryParameter('id', $environment->getInputProvider()->getParameter('pid'));
+        }
+
+        $redirectEvent = new RedirectEvent($redirectUrl->getUrl());
+        $environment->getEventDispatcher()->dispatch(ContaoEvents::CONTROLLER_REDIRECT, $redirectEvent);
+    }
+
+    /**
+     * @param ModelIdInterface     $modelId
+     * @param EnvironmentInterface $environment
+     *
+     * @return ModelInterface
+     */
+    protected function copyParent(ModelIdInterface $modelId, EnvironmentInterface $environment)
+    {
+        $dataProvider       = $environment->getDataDefinition();
+        $parentDataProvider = $environment->getParentDataDefinition();
+
+        $copyEnvironment = clone $environment;
+        $copyEnvironment->setDataDefinition($parentDataProvider);
+        $copyEnvironment->setParentDataDefinition($dataProvider);
+
+        $copyControllerEnvironment = $copyEnvironment->getController()->getEnvironment();
+        $copyControllerEnvironment->setDataDefinition($parentDataProvider);
+        $copyControllerEnvironment->setParentDataDefinition($dataProvider);
+
+        return $this->copyHandler($modelId, $copyEnvironment);
+    }
+
+    /**
+     * @param ModelIdInterface     $modelId
+     * @param EnvironmentInterface $environment
+     *
+     * @return ModelInterface
+     */
+    protected function copyHandler(ModelIdInterface $modelId, EnvironmentInterface $environment)
+    {
+        $copyHandler = new CopyHandler();
+        $copyHandler->setEnvironment($environment);
+
+        return $copyHandler->copy($modelId);
+    }
+
+    /**
+     * @param ModelIdInterface     $modelId
+     * @param ModelInterface       $copiedModel
+     * @param EnvironmentInterface $environment
+     */
+    protected function copyEachChilds(ModelIdInterface $modelId, ModelInterface $copiedModel, EnvironmentInterface $environment)
+    {
+        $childDataProviderName = $environment->getInputProvider()->getParameter('ctable');
+        $dataProvider          = $environment->getDataProvider();
+        $childDataProvider     = $environment->getDataProvider($childDataProviderName);
+        $modelRelationship     = $environment->getParentDataDefinition()->getModelRelationshipDefinition();
+
+        $childCondition = $modelRelationship->getChildCondition(
+            $copiedModel->getProviderName(),
+            $childDataProviderName
+        );
+        if (!$childCondition) {
+            return;
+        }
+
+        $parentModel   = $dataProvider->fetch($dataProvider->getEmptyConfig()->setId($modelId->getId()));
+        $parentFilters = $childCondition->getFilter($parentModel);
+        $copiedFilters = $childCondition->getFilter($copiedModel);
+        $filter        = array();
+        // Todo can many filter has operation equal
+        foreach ($parentFilters as $index => $parentFilter) {
+            if ($parentFilter['operation'] !== '=') {
+                continue;
+            }
+
+            $filter = array(
+                'parent' => $parentFilter,
+                'copied' => $copiedFilters[$index],
+            );
+        }
+
+        $childModels = $childDataProvider->fetchAll(
+            $childDataProvider->getEmptyConfig()->setFilter(array($filter['parent']))
+        );
+        if ($childModels->count() < 1) {
+            return;
+        }
+
+        foreach ($childModels->getModelIds() as $index => $modelId) {
+            $childModel  = $childModels->get($index);
+            $copyModelId = ModelId::fromModel($childModel);
+
+            $copiedChildModel = null;
+            if ($environment->getDataDefinition()->getName() !== $copyModelId->getDataProviderName()) {
+                $copiedChildModel = $this->copyParent($copyModelId, $environment);
+            }
+
+            if ($environment->getDataDefinition()->getName() === $copyModelId->getDataProviderName()) {
+                $copiedChildModel = $this->copyHandler($copyModelId, $environment);
+            }
+
+            $copiedChildModel->setProperty(
+                $filter['copied']['property'],
+                $filter['copied']['value']
+            );
+
+            $childDataProvider->save($copiedChildModel);
+        }
     }
 }
