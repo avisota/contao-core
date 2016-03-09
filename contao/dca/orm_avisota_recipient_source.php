2d640459eb25621f590d83780395ed0bae227ed9
Sven Baumann <baumann.sv@gmail.com>
2016-03-09 17:31:53 +0100

    give recipient source view edit all

--- contao/dca/orm_avisota_recipient_source.php
+++ contao/dca/orm_avisota_recipient_source.php
@@ -57,15 +57,14 @@ $GLOBALS['TL_DCA']['orm_avisota_recipient_source'] = array
             'format' => '%s <span style="color:#b3b3b3; padding-left:3px;">[%s]</span>'
         ),
         'global_operations' => array
-        (/*
-			'all' => array
-			(
-				'label'      => &$GLOBALS['TL_LANG']['MSC']['all'],
-				'href'       => 'act=select',
-				'class'      => 'header_edit_all',
-				'attributes' => 'onclick="Backend.getScrollOffset();" accesskey="e"'
-			)
-			*/
+        (
+            'all' => array
+            (
+                'label'      => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'       => 'act=select',
+                'class'      => 'header_edit_all',
+                'attributes' => 'onclick="Backend.getScrollOffset();" accesskey="e"'
+            )
         ),
         'operations'        => array
         (
